5f6ea5a77c6459b8025b5397428a0c5b59f2a1ef
hyunjun529 <hyunjun529@gmail.com>
2017-11-13 16:09:15 +0900

    reset Project, this project worked PlaneDetection

---
+++ TestParameters/TestParameters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestParameters
+//
+//  Created by hyunjun529 on 2017. 11. 13..
+//  Copyright © 2017년 hyunjun529. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestParameters/TestParameters/ViewController.m
@@ -0,0 +1,191 @@
+//
+//  ViewController.m
+//  TestParameters
+//
+//  Created by hyunjun529 on 2017. 11. 13..
+//  Copyright © 2017년 hyunjun529. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <ARSCNViewDelegate>
+
+@property (nonatomic, strong) IBOutlet ARSCNView *sceneView;
+
+@end
+
+    
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Set the view's delegate
+    self.sceneView.delegate = self;
+    
+    // Show statistics such as fps and timing information
+    self.sceneView.showsStatistics = YES;
+    
+    // Create a new scene
+    SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/ship.scn"];
+    
+    
+    // h529 : add Debug World Origin
+    self.sceneView.debugOptions = ARSCNDebugOptionShowWorldOrigin;
+    
+    
+    // h529 : add Cube
+    // The 3D cube geometry we want to draw
+    SCNBox *boxGeometry = [SCNBox
+                           boxWithWidth:0.1
+                           height:0.1
+                           length:0.1
+                           chamferRadius:0.0];
+    
+    int n = 4;
+    SCNNode *boxNode[n];
+    for(int i =0; i< n; i++){
+        boxNode[i] = [SCNNode nodeWithGeometry:boxGeometry];
+    }
+    
+    // Position the box just in front of the camera
+    boxNode[0].position = SCNVector3Make(1, 0, 1);
+    boxNode[1].position = SCNVector3Make(1, 0, -1);
+    boxNode[2].position = SCNVector3Make(-1, 0, 1);
+    boxNode[3].position = SCNVector3Make(-1, 0, -1);
+    
+    for(int i =0; i< n; i++){
+        [scene.rootNode addChildNode: boxNode[i]];
+    }
+    
+    
+    // h529 : add visualize PlaneDetection
+     self.planes = [NSMutableDictionary new];
+    
+    
+    // h529 : autoLighting
+    self.sceneView.autoenablesDefaultLighting = YES;
+    
+    
+    // Set the scene to the view
+    self.sceneView.scene = scene;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    // Create a session configuration
+    ARWorldTrackingConfiguration *configuration = [ARWorldTrackingConfiguration new];
+
+    // Specify that we do want to track horizontal planes. Setting this will cause the ARSCNViewDelegate
+    // methods to be called when scenes are detected
+    configuration.planeDetection = ARPlaneDetectionHorizontal;
+    
+    // Run the view's session
+    [self.sceneView.session runWithConfiguration:configuration];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    // Pause the view's session
+    [self.sceneView.session pause];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - ARSCNViewDelegate
+
+/*
+// Override to create and configure nodes for anchors added to the view's session.
+- (SCNNode *)renderer:(id<SCNSceneRenderer>)renderer nodeForAnchor:(ARAnchor *)anchor {
+    SCNNode *node = [SCNNode new];
+ 
+    // Add geometry to the node...
+ 
+    return node;
+}
+*/
+
+
+
+/**
+ Called when a new node has been mapped to the given anchor.
+ 
+ @param renderer The renderer that will render the scene.
+ @param node The node that maps to the anchor.
+ @param anchor The added anchor.
+ */
+- (void)renderer:(id <SCNSceneRenderer>)renderer didAddNode:(SCNNode *)node forAnchor:(ARAnchor *)anchor {
+    if (![anchor isKindOfClass:[ARPlaneAnchor class]]) {
+        return;
+    }
+    
+    // When a new plane is detected we create a new SceneKit plane to visualize it in 3D
+    Plane *plane = [[Plane alloc] initWithAnchor: (ARPlaneAnchor *)anchor];
+    [self.planes setObject:plane forKey:anchor.identifier];
+    [node addChildNode:plane];
+}
+
+/**
+ Called when a node has been updated with data from the given anchor.
+ 
+ @param renderer The renderer that will render the scene.
+ @param node The node that was updated.
+ @param anchor The anchor that was updated.
+ */
+- (void)renderer:(id <SCNSceneRenderer>)renderer didUpdateNode:(SCNNode *)node forAnchor:(ARAnchor *)anchor {
+    Plane *plane = [self.planes objectForKey:anchor.identifier];
+    if (plane == nil) {
+        return;
+    }
+    
+    // When an anchor is updated we need to also update our 3D geometry too. For example
+    // the width and height of the plane detection may have changed so we need to update
+    // our SceneKit geometry to match that
+    [plane update:(ARPlaneAnchor *)anchor];
+}
+
+/**
+ Called when a mapped node has been removed from the scene graph for the given anchor.
+ 
+ @param renderer The renderer that will render the scene.
+ @param node The node that was removed.
+ @param anchor The anchor that was removed.
+ */
+- (void)renderer:(id <SCNSceneRenderer>)renderer didRemoveNode:(SCNNode *)node forAnchor:(ARAnchor *)anchor {
+    // Nodes will be removed if planes multiple individual planes that are detected to all be
+    // part of a larger plane are merged.
+    [self.planes removeObjectForKey:anchor.identifier];
+}
+
+/**
+ Called when a node will be updated with data from the given anchor.
+ 
+ @param renderer The renderer that will render the scene.
+ @param node The node that will be updated.
+ @param anchor The anchor that was updated.
+ */
+- (void)renderer:(id <SCNSceneRenderer>)renderer willUpdateNode:(SCNNode *)node forAnchor:(ARAnchor *)anchor {
+}
+
+
+- (void)session:(ARSession *)session didFailWithError:(NSError *)error {
+    // Present an error message to the user
+    
+}
+
+- (void)sessionWasInterrupted:(ARSession *)session {
+    // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    
+}
+
+- (void)sessionInterruptionEnded:(ARSession *)session {
+    // Reset tracking and/or remove existing anchors if consistent tracking is required
+    
+}
+
+@end
